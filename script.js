dc46c10ff9e4b4cabc62e77a98d7314e747bbe53
Daniel Alvarez <alvarez0daniel@gmail.com>
2022-02-26 11:53:30 -0800

    initialized worker

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ async function setup() {
   // connect webcam stream to video element src
   const stream = await navigator.mediaDevices.getUserMedia({ video:true });
   video.srcObject = stream;
+
+  // add event listener on play
+  video.addEventListener('playing', async () => {
+    // initialize Tesseract worker
+    const worker = Tesseract.createWorker();
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+  });
 }
 setup();
