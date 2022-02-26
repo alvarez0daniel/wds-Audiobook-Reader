a8badfa69418f1dda398d3b095fc7f03b866fa0c
Daniel Alvarez <alvarez0daniel@gmail.com>
2022-02-26 11:48:17 -0800

    added some comments to setup()

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const video = document.querySelector('video');
 
 async function setup() {
+  // connect webcam stream to video element src
   const stream = await navigator.mediaDevices.getUserMedia({ video:true });
   video.srcObject = stream;
 }
